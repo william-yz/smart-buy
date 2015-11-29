ffa3b5ef977eb1604feb1654cd324dcbdc162654
William <545792860@qq.com>
2015-11-29 22:35:34 +0800

    to-do : need to login by using cheerio. send all data from login form..

--- spider/taobao/runner.js
+++ spider/taobao/runner.js
@@ -4,27 +4,49 @@
 'use strict';
 
 var superagent = require('superagent'),
-    fs = require('fs');
+    fs = require('fs'),
+    moment = require('moment');
 
 var config = require('./config'),
     db = require('../db/dbFacade');
 
 var homeSearchUrl = config.homeSearchUrl,
     rgExpToMatch = new RegExp(config.rgExpToMatch),
-    rgExpToReplace = new RegExp(config.rgExpToReplace);
+    rgExpToReplace = new RegExp(config.rgExpToReplace),
+    pagesToSearch = config.pagesToSearch,
+    pageDataValue = config.pageDataValue;
 
-var seachItem = '篮球';
-superagent.get(homeSearchUrl+'?q='+seachItem + '&data-key=s&data-value=66' )
-    .end(function(err, res) {
-        var text = res.text.match(rgExpToMatch).toString();
-        text = text.substring(0,text.length-1);
-        text = text.replace(rgExpToReplace,'');
-        text = text.replace(';','###');
+var searchItem = '篮球';
+for (var i = 0; i < pagesToSearch; i ++) {
+    var dataValue = pageDataValue * i;
+    superagent.get(homeSearchUrl+'?q='+searchItem + '&data-key=s&data-value=' + dataValue )
+        .end(function(err, res) {
+            if (err) {
+                console.log(err);
+            } else {
+                var text = res.text.match(rgExpToMatch);
+                if (text) {
+                    text = text.toString();
+                } else {
+                    console.log(res.text);
+                    return;
+                }
+                text = text.substring(0,text.length-1);
+                text = text.replace(rgExpToReplace,'');
+                var parsedText = JSON.parse(text),
+                    itemInfoList = parsedText.mods.itemlist.data.auctions,
+                    today = moment().format('YYYYMMDD'); 
+                itemInfoList.forEach(function(itemInfo) {
+                    itemInfo.searchItem = searchItem;
+                });
+                db.insert('originData' + today, itemInfoList);
+                // fs.writeFile('./test', text, function(){
+                //     console.log('done!');
+                // });
+                // 
+                // console.info(t.mods.itemlist.data.auctions);
+            }
+            
+        });
+}
 
-        db.insert('odatatest', text);
-        // fs.writeFile('./test', text, function(){
-        //     console.log('done!');
-        // });
-        // var t = JSON.parse(text);
-        // console.info(t.mods.itemlist.data.auctions);
-    });
